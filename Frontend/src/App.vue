b95f47235c787ada803ecc6cca9de90d4ca9f6a4
dyqiuka <khuongduyhltb@gmail.com>
2026-06-09 13:47:19 +0700

    9/6

---
+++ Frontend/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app>
+    <v-app-bar color="primary" elevation="1">
+      <v-app-bar-title>Training Center</v-app-bar-title>
+      <v-btn to="/" variant="text">Trang chu</v-btn>
+      <v-btn to="/courses" variant="text">Khoa hoc</v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
---
+++ Frontend/src/main.js
@@ -0,0 +1,15 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+
+import App from './App.vue'
+import router from './router'
+import vuetify from './plugins/vuetify'
+import './styles/main.css'
+
+createApp(App)
+  .use(createPinia())
+  .use(router)
+  .use(vuetify)
+  .mount('#app')
